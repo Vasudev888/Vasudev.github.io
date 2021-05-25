672db71ee8eb49b74a10a41e08c76cda69dfbc54
Vasudev Kulkarni <vdk5384@gmail.com>
2021-05-25 20:30:45 +0530

    project page changes and embed light box

--- scroll.js
+++ scroll.js
@@ -3,7 +3,7 @@ $('.post-wrapper').slick({
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    dots: true,
+    dots: false,
     pauseOnHover: true,
     nextArrow: $('.next'),
     prevArrow: $('.prev'),
